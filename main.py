0a237659c514f116dfc642ff01b76fb37f936ff0
Olha <1helga.starlight1@gmail.com>
2025-01-28 09:56:42 +0100

    second code commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def common_elements():
-    numbers = set()
+    list_3 = [x for x in range(100) if x % 3 == 0]
+    list_5 = [x for x in range(100) if x % 5 == 0]
 
-    for x in range(100):
-        if x % 3 == 0 and x % 5 == 0:
-            numbers.add(x)
-    return numbers
+    set_3 = set(list_3)
+    set_5 = set(list_5)
+    return set_3 & set_5
 
 
 assert common_elements() == {0, 75, 45, 15, 90, 60, 30}, "Test failed!"
